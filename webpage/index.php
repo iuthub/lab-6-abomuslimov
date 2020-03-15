0e5200939fcd6937ea6f48ff76b1ec4336bdcd71
abomuslimov <45311775+abomuslimov@users.noreply.github.com>
2020-03-15 23:36:25 +0500

    Lab 6 has been finished

--- webpage/index.php
+++ webpage/index.php
@@ -13,19 +13,28 @@
 		</p>
 		
 		<hr />
-		
-		<h2>Please, fill below fields correctly</h2>
+        <h2>Please, fill below fields correctly</h2>
 		<dl>
 			<dt>Name</dt>
 			<dd>
 				???
 			</dd>
-			
+
 			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
 		</dl>
 		
 		<div>
 			Register
 		</div>
+        <div class="newOne">
+            <p>Mrs. Lowey (for herself, Mr. Scott of Virginia, Mr. Neal, Mr. Bishop of Georgia, Ms. DeLauro, Mr. Pallone, and Mr. Peterson) introduced the following bill; which was referred to the Committee on Appropriations, and in addition to the Committees on the Budget, and Ways and Means, for a period to be subsequently determined by the Speaker, in each case for consideration of such provisions as fall within the jurisdiction of the committee concerned</p>
+        </div>
+        <div class="oldOne">
+        	<p>For an additional amount for the “Commodity Assistance Program” for the emergency food assistance program as authorized by section 27(a) of the Food and Nutrition Act of 2008 (7 U.S.C. 2036(a)) and section 204(a)(1) of the Emergency Food Assistance Act of 1983 (7 U.S.C. 7508(a)(1)), $400,000,000, to remain available through September 30, 2021: Provided, That of the funds made available, the Secretary may use up to $100,000,000 for costs associated with the distribution of commodities: Provided further, That such amount is designated by the Congress as being for an emergency requirement pursuant to section 251(b)(2)(A)(i) of the Balanced Budget and Emergency Deficit Control Act of 1985.</p>
+<p>Funding.—There are hereby appropriated to the Secretary of Agriculture such amounts as are necessary to carry out this section: Provided, That such amount is designated by the Congress as being for an emergency requirement pursuant to section 251(b)(2)(A)(i) of the Balanced Budget and Emergency Deficit Control Act of 1985.
+
+Sec. 102. In addition to amounts otherwise made available, $100,000,000, to remain available through September 30, 2021, shall be available for the Secretary of Agriculture to provide grants to the Commonwealth of the Northern Mariana Islands, Puerto Rico, and American Samoa for nutrition assistance in response to a COVID–19 public health emergency: Provided, That such amount is designated by the Congress as being for an emergency requirement pursuant to section 251(b)(2)(A)(i) of the Balanced Budget and Emergency Deficit Control Act of 1985.</p>
+        </div>
+	
 	</body>
 </html>
--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -21,6 +21,20 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 					}
 }
 
+$pattern = "/ca[kf]e/";
+$text = "He was eating cake in the cafe.";
+if (preg_match($pattern, $text)) {
+    echo "Match found!";
+} else {
+    echo "Match not found.";
+}
+
+
+$pattern = "/ca[kf]e/";
+$text = "He was eating cake in the cafe.";
+$matches = preg_match_all($pattern, $text, $array);
+echo $matches . " matches were found.";
+
 ?>
 
 
